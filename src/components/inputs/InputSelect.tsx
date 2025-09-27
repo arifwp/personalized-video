ab19303d98374514a027afe0fde5a6026b9f32a4
Arif Wahyu <arifwahyu696@gmail.com>
2025-09-28 03:35:14 +0700

    reusable components

---
+++ src/components/inputs/InputSelect.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Actor } from "@/constants/actors";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { ChevronUpDownIcon } from "@heroicons/react/16/solid";
+import { CheckIcon } from "@heroicons/react/20/solid";
+
+export const InputSelect = ({
+  onChange,
+  value,
+  options,
+  isLoading = false,
+}: {
+  onChange: (data: Actor) => void;
+  value: Actor;
+  options: Actor[];
+  isLoading?: boolean;
+}) => {
+  return (
+    <Listbox value={value} onChange={onChange} disabled={isLoading}>
+      <div className="relative sm:max-w-md">
+        <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6">
+          <span className="col-start-1 row-start-1 truncate pr-6">
+            {value?.name}
+          </span>
+          <ChevronUpDownIcon
+            aria-hidden="true"
+            className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+          />
+        </ListboxButton>
+
+        <ListboxOptions
+          transition
+          className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg outline-1 outline-black/5 data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
+        >
+          {options.map((person) => (
+            <ListboxOption
+              key={person.id}
+              value={person}
+              className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none data-focus:bg-indigo-600 data-focus:text-white data-focus:outline-hidden"
+            >
+              <span className="block truncate font-normal group-data-selected:font-semibold">
+                {person.name}
+              </span>
+
+              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-not-data-selected:hidden group-data-focus:text-white">
+                <CheckIcon aria-hidden="true" className="size-5" />
+              </span>
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </div>
+    </Listbox>
+  );
+};
