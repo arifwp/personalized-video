4647dc94d1f7b051a86c0e74102c7e8b3d1dfe76
Arif Wahyu <arifwahyu696@gmail.com>
2025-09-28 03:35:34 +0700

    initiate

---
+++ src/constants/actors.ts
@@ -0,0 +1,10 @@
+export interface Actor {
+  id: number;
+  name: string;
+}
+
+export const mockActors: Actor[] = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Carlos Vega" },
+];
